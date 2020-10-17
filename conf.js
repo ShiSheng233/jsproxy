2741f247081aaf2697bf98d274490e4bdfe640c6
Shi_Sheng <39020034+ShiSheng233@users.noreply.github.com>
2020-10-17 21:47:56 +0800

    233

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '233',
+  ver: '2333',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -12,9 +12,8 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    },
     'mysite': {
-      label: '当前站点',
+      label: 'CloudFlare节点',
       lines: {
         [location.host]: 1,
       }
@@ -52,7 +51,7 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   assets_cdn: 'assets/',
@@ -71,5 +70,6 @@ jsproxy_config({
   /**
    * URL 自定义处理（设计中）
    */
-  url_handler: {}
+  url_handler: {
+  }
 })
