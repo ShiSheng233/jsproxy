4fec4b1e9e2d37bdb7e015dde14cfa6d4d56e606
Shi_Sheng <39020034+ShiSheng233@users.noreply.github.com>
2021-01-09 22:51:24 +0800

    fix sth

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '23333',
+  ver: '233333',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -20,18 +20,14 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
-      hidden: true,
+      label: '备用节点',
+      hidden: false,
       lines: {
-        // 收费版（高权重）
-        //'node-cfworker-2.etherdream.com': 4,
-
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
-        //'b.jsproxy.workers.dev': 1,
+        'b.007.workers.dev': 1,
+        'b.lulu.workers.dev': 1,
+        'b.jsproxy.workers.dev': 1,
       }
     }
   },
@@ -51,7 +47,7 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/shisheng233/jsproxy@gh-pages/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   assets_cdn: 'assets/',
