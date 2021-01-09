14bc4bdbcfb43a470211447ad98baf5f5d655eff
Shi_Sheng <39020034+ShiSheng233@users.noreply.github.com>
2021-01-09 23:01:01 +0800

    fix again and again

--- conf.js
+++ conf.js
@@ -17,18 +17,6 @@ jsproxy_config({
       lines: {
         [location.host]: 1,
       }
-    },
-    // 该节点用于加载大体积的静态资源
-    'cfworker': {
-      label: '备用节点',
-      hidden: false,
-      lines: {
-        // 免费版（低权重，分摊一些成本）
-        // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
-      }
     }
   },
 
